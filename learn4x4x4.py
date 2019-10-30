ccfe3d6a581141e32fc70c99c18f12dcf96fae41
Ewan McKinley <ewan@live.unc.edu>
2019-10-30 13:21:10 -0400

    created board, commented out tile

--- learn4x4x4.py
+++ learn4x4x4.py
@@ -1,6 +1,35 @@
 #!/Library/Frameworks/Python.framework/Versions/2.7/bin/python
 import sys
+import pprint
+
+#class Tile:
+#	def __init__(self, state):
+#		self.state = state;
+#	
+#	def get_state(self):
+#		return self.state
+#	
+#	def set_state(self, newState):
+#		self.state = newState
+
+class Board:
+	def __init__(self):
+		self.board = [[[None for x in range(4)] for y in range(4)] for z in range(4)]				
+
+	def print_board(self):
+		pprint.pprint(self.board)
+		
 
 trialsCount = [sys.argv[1], sys.argv[2], sys.argv[3]];
-for x in trialsCount:
-	print(x)
+
+# for x in trialsCount:
+#	print(x)
+
+# testTile = Tile("X")
+# print(testTile.state)
+# testTile.set_state("O")
+# print(testTile.state)
+# testTile2 = Tile(None)
+# print(testTile2.state)
+testBoard = Board()
+testBoard.print_board()
