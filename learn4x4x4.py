fae1b5d8da819cf9bb5b3b1b77878e7af60c1bac
Ewan McKinley <ewan@live.unc.edu>
2019-11-03 18:31:29 -0500

    added preliminary reward structure and cleaned code

--- learn4x4x4.py
+++ learn4x4x4.py
@@ -2,34 +2,40 @@
 import sys
 import pprint
 
-#class Tile:
-#	def __init__(self, state):
-#		self.state = state;
-#	
-#	def get_state(self):
-#		return self.state
-#	
-#	def set_state(self, newState):
-#		self.state = newState
+class Position:
+    def __init__(self, x, y, z):
+        this.x = x;
+        this.y = y;
+        this.z = z;
 
 class Board:
-	def __init__(self):
-		self.board = [[[None for x in range(4)] for y in range(4)] for z in range(4)]				
+    def __init__(self):
+    	self.board = [[[None for x in range(4)] for y in range(4)] for z in range(4)]
+        self.x_positions = []
+        self.y_positions = []
 
-	def print_board(self):
-		pprint.pprint(self.board)
-		
+    def print_board(self):
+    	pprint.pprint(self.board)
+
+
+#sums up rewards of making a move in a position on the board
+def find_rewards(board, player, position):
+    reward_sum = 0;
+    player_positions = []
+    if player is "X":
+        player_positions = board.x_positions
+    else
+        player_positions = board.x_positions
+
+    for current_positions in player_positions:
+        if current_positions.x == position.x:
+            reward_sum += 1
+        if current_positions.y == position.y:
+            reward_sum += 1
+        if current_positions.z == position.z:
+            reward_sum += 1
+
+    return reward_sum
 
 trialsCount = [sys.argv[1], sys.argv[2], sys.argv[3]];
 
-# for x in trialsCount:
-#	print(x)
-
-# testTile = Tile("X")
-# print(testTile.state)
-# testTile.set_state("O")
-# print(testTile.state)
-# testTile2 = Tile(None)
-# print(testTile2.state)
-testBoard = Board()
-testBoard.print_board()
