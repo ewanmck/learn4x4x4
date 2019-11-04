f8ae0d493beb9e51006df48d33d74b310fe05c2f
Ewan McKinley <ewan@live.unc.edu>
2019-11-03 19:22:57 -0500

    added terminal case check

--- learn4x4x4.py
+++ learn4x4x4.py
@@ -4,9 +4,9 @@ import pprint
 
 class Position:
     def __init__(self, x, y, z):
-        this.x = x;
-        this.y = y;
-        this.z = z;
+        this.x = x
+        this.y = y
+        this.z = z
 
 class Board:
     def __init__(self):
@@ -20,7 +20,7 @@ class Board:
 
 #sums up rewards of making a move in a position on the board
 def find_rewards(board, player, position):
-    reward_sum = 0;
+    reward_sum = 0
     player_positions = []
     if player is "X":
         player_positions = board.x_positions
@@ -37,5 +37,85 @@ def find_rewards(board, player, position):
 
     return reward_sum
 
-trialsCount = [sys.argv[1], sys.argv[2], sys.argv[3]];
+def find_terminal(board, player):
+    #find across rows
+    for z in range(4):
+        for y in range(4):
+            for x in range(4):
+                assumption = true
+                if board.board[x][y][z] != player:
+                    assumption = false
+                if assumption
+                    return true
+
+
+    #find across columns
+    for z in range(4):
+        for x in range(4):
+            for y in range(4):
+                assumption = true
+                if board.board[x][y][z] != player:
+                    assumption = false
+                if assumption:
+                    return true
+
+    #find across verticals
+    for x in range(4):
+        for y in range(4):
+            for z in range(4):
+                assumption = true
+                if board.board[x][y][z] != player:
+                    assumption = false
+                if assumption:
+                    return true
+
+    #find across 2D diagonals
+    for z in range(4):
+        for x in range(4):
+            assumption = true
+            if board.board[x][x][z] != player:
+                assumption = false
+            if assumption:
+                return true
+        for x in range(4):
+            assumption = true
+            if board.board[4-x][x][z] != player:
+                assumption = false
+            if assumption:
+                return true
+
+    #find across 3D diagonals
+    for x in range(4):
+        assumption = true
+        if board.board[x][x][x] != player
+            assumption = false
+        if assumption:
+            return true
+
+    for x in range(4):
+        assumption = true
+        if board.board[x][x][4-x] != player
+            assumption = false
+        if assumption:
+            return true
+
+    for x in range(4):
+        assumption = true
+        if board.board[x][4-x][x] != player
+            assumption = false
+        if assumption:
+            return true
+
+    for x in range (4):
+        assumption = true
+        if board.board[x][4-x][4-x] != player
+            assumption = false
+        if assumption:
+            return true
+
+    #return false if none of the above cases were true
+    return false
+
+
+trialsCount = [sys.argv[1], sys.argv[2], sys.argv[3]]
 
