74b8c9bb79d9c277e2ff4117baaa91b8371ae8eb
Ewan McKinley <ewan@live.unc.edu>
2019-11-11 20:27:46 -0500

    functional creation of q table

--- learn4x4x4.py
+++ learn4x4x4.py
@@ -1,5 +1,6 @@
 #!/Library/Frameworks/Python.framework/Versions/2.7/bin/python
 import sys
+import random
 import pprint
 
 class Position:
@@ -12,7 +13,7 @@ class Board:
     def __init__(self):
     	self.board = [[[None for x in range(4)] for y in range(4)] for z in range(4)]
         self.x_positions = []
-        self.y_positions = []
+        self.o_positions = []
 
     def print_board(self):
     	pprint.pprint(self.board)
@@ -25,7 +26,7 @@ def find_rewards(board, player, position):
     if player is "X":
         player_positions = board.x_positions
     else:
-        player_positions = board.x_positions
+        player_positions = board.o_positions
 
     for current_positions in player_positions:
         if current_positions.x == position.x:
@@ -155,6 +156,18 @@ def set_num_tables(board):
             for x in range(4):
                 board.board[x][y][z] = 0
 
+def random_selector(board, position, alpha):
+    rand = random.random()
+    if (rand > alpha):
+        return position
+    else:
+        x = random.randint(0,3)
+        y = random.randint(0,3)
+        z = random.randint(0,3)
+        if board.board[x][y][z] is None:
+            return Position(x,y,z)
+        else:
+            return random_selector(board, position, alpha)
 
 trialsCount = [int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3])]
 
@@ -165,13 +178,15 @@ set_num_tables(q_count)
 set_num_tables(n_count)
 
 alpha = .7
-discount = .7
-for x in range(trialsCount[2]):
-    if x is trialsCount[0] or x is trialsCount[1] or x is trialsCount[2]:
+discount = .2
+for trials in range(trialsCount[2]):
+    if trials == trialsCount[0] or trials == trialsCount[1] or trials == trialsCount[2]-1:
         q_count.print_board()
     board = Board()
     player_1 = "X"
     player_2 = "O"
+    last_pos_X = Position(None, None, None)
+    last_pos_O = Position(None, None, None)
     while not find_terminal(board, "X") or not find_terminal(board, "O"):
         for z in range (4):
             for y in range(4):
@@ -179,23 +194,41 @@ for x in range(trialsCount[2]):
                     if board.board[x][y][z] is None:
                         position = Position(x,y,z)
                         max_q_prime = find_max_reward(board, "X")
+                        max_q_prime_val = q_count.board[max_q_prime.x][max_q_prime.y][max_q_prime.z]
                         n_count.board[x][y][z] = n_count.board[x][y][z] + 1
-                        q_count.board[x][y][z] = q_count.board[x][y][z] + alpha * n_count.board[x][y][z] * (find_rewards(board, "X", position) + discount * q_count.board[max_q_prime.x][max_q_prime.y][max_q_prime.z] - q_count.board[x][y][z])
-        set_position_X = find_max_q(board, "X")
+
+                        prev_q = 0
+                        if last_pos_X.x is not None:
+                            prev_q = q_count.board[last_pos_X.x][last_pos_X.y][last_pos_X.z]
+
+                        q_count.board[x][y][z] = (1 - alpha) *  q_count.board[x][y][z] + alpha * (find_rewards(board, "X", position) + discount * max_q_prime_val - prev_q)
+
+        set_position_X = random_selector(board, find_max_q(board, "X"), alpha)
         board.board[set_position_X.x][set_position_X.y][set_position_X.z] = "X"
-        q_count.print_board()
+        board.x_positions.append(set_position_X)
+        last_pos_X = set_position_X
+
         if find_terminal(board, "X") or find_terminal(board, "O"):
             break
+
         for z in range (4):
             for y in range(4):
                 for x in range(4):
                     if board.board[x][y][z] is None:
                         position = Position(x,y,z)
                         max_q_prime = find_max_reward(board, "O")
+                        max_q_prime_val = q_count.board[max_q_prime.x][max_q_prime.y][max_q_prime.z]
                         n_count.board[x][y][z] = n_count.board[x][y][z] + 1
-                        q_count.board[x][y][z] = q_count.board[x][y][z] + alpha * n_count.board[x][y][z] * (find_rewards(board, "O", position) + discount * q_count.board[max_q_prime.x][max_q_prime.y][max_q_prime.z] - q_count.board[x][y][z])
-        set_position_O = find_max_q(board, "O")
+
+                        prev_q = 0
+                        if last_pos_O.x is not None:
+                            prev_q = q_count.board[last_pos_O.x][last_pos_O.y][last_pos_O.z]
+
+                        q_count.board[x][y][z] = (1 - alpha) *  q_count.board[x][y][z] + alpha * (find_rewards(board, "O", position) + discount * max_q_prime_val - prev_q)
+
+        set_position_O = random_selector(board, find_max_q(board, "O"), alpha)
         board.board[set_position_O.x][set_position_O.y][set_position_O.z] = "O"
-        q_count.print_board()
-    board.print_board()
+        board.o_positions.append(set_position_O)
+        last_pos_O = set_position_O
+    alpha = alpha / (1 + .1)
 q_count.print_board()
